2851942f64a9c763ca7742c96792a7884210f5b2
Cook1619 <cook6348@gmail.com>
2020-05-19 14:52:44 -0500

    Setting up the module and testing out expressions in angular

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
       {{ title }}
+      <div>
+        {{ numberOne + numberTwo }}
+      </div>
+      <div>
+        {{ isHappy ? ':-)' : ':-('}}
+      </div>
     </div>
-  `
+  `,
 })
 export class AppComponent {
   title: string;
-  list: []
-  constructor(){
-    this.title = 'Angular Starter App'
-    this.list = []
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  isHappy: boolean =  true
+  constructor() {
+    this.title = "Angular Starter App";
   }
 }
